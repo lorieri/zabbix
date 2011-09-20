a32fa64a83f5a3decbd26bbedfccc36a4079c77e
lorieri <lorieri@gmail.com>
2011-09-20 14:50:38 -0300

    updated auto-register curl script

--- chef/cookbooks/zabbix-aws/files/default/auto-register/zabbix_autoregister_curl.sh
+++ chef/cookbooks/zabbix-aws/files/default/auto-register/zabbix_autoregister_curl.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-echo "OUTDATED"
-exit 
-
 #Testing argument
 test -z "$1" && { echo "Usage: zabbix_register_api.sh  [mytag]"; exit 0; }
 
@@ -15,29 +12,28 @@ USER='user'
 PASSWORD='password'
 
 #machine's amazon informations
-DNS=`wget --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/hostname`
-ISID=`wget --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/instance-id`
+DNS=`curl --connect-timeout 15 -s http://169.254.169.254/latest/meta-data/hostname`
+ISID=`curl --connect-timeout 15 -s http://169.254.169.254/latest/meta-data/instance-id`
 
 #testing amazon informations
 test -z "$DNS" && { echo "Erro ao pegar DNS"; exit 0; }
 test -z "$ISID" && { echo "Erro ao pegar Instance-id";  exit 0; }
 
-
 echo "-> Authenticating..."
-AUTH=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" $URL -d '{"jsonrpc":"2.0","method":"user.authenticate","params":{"user":"'"$USER"'","password":"'"$PASSWORD"'"},"id": 1}'|sed -n 's/.*"result":"\([a-f0-9]*\)".*/\1/p'`
+AUTH=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" "$URL" -d '{"jsonrpc":"2.0","method":"user.authenticate","params":{"user":"'"$USER"'","password":"'"$PASSWORD"'"},"id": 1}' |sed -n 's/.*"result":"\([a-f0-9]*\)".*/\1/p'`
 
 #Testing Auth
 test -z "$AUTH" && { echo "Error authenticating" ; exit 0 ; }
 
 echo "-> Searching hostgroups..."
 # From tag's group and Discovered hosts
-GROUPS=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" $URL -d '{"jsonrpc":"2.0","method":"hostgroup.get","params":{"filter":{"name": ["'"$MYTAG"'","Discovered hosts"]}},"id": 1,"auth":"'"$AUTH"'"}' | sed -n 's/.*"result":\[\([^]]*\)\].*/\1/p'`
+GRPS=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" "$URL" -d '{"jsonrpc":"2.0","method":"hostgroup.get","params":{"filter":{"name": ["'"$MYTAG"'","Discovered hosts"]}},"id": 1,"auth":"'"$AUTH"'"}' |sed -n 's/.*"result":\[\([^]]*\)\].*/\1/p'`
 
 #checking groups 
-echo "$GROUPS" |grep -o groupid|wc -l|grep -qx 2 || { echo "Error getting groups"; exit 0 ; }
+echo "$GRPS" |grep -o groupid|wc -l|grep -qx 2 || { echo "Error getting groups"; exit 0 ; }
 
 echo "-> Getting LinuxBasic and Tag's template..."
-TEMPLATES=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" $URL -d '{"jsonrpc":"2.0","method":"template.get","params":{"filter":{"host":["Template_LinuxBasic","Template_'"$MYTAG"'"]}},"id": 1,"auth":"'"$AUTH"'"}'| sed -n 's/.*"result":\[\([^]]*\)\].*/\1/;s/"hostid":"[^"]*",//gp'`
+TEMPLATES=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" "$URL" -d '{"jsonrpc":"2.0","method":"template.get","params":{"filter":{"host":["Template_LinuxBasic","Template_'"$MYTAG"'"]}},"id": 1,"auth":"'"$AUTH"'"}' |sed -n 's/.*"result":\[\([^]]*\)\].*/\1/;s/"hostid":"[^"]*",//gp'`
 
 #checking templates
 echo "$TEMPLATES" |grep -o templateid|wc -l|grep -qx 2 || { echo "Error getting templates"; exit 0 ; }
@@ -45,7 +41,7 @@ echo "$TEMPLATES" |grep -o templateid|wc -l|grep -qx 2 || { echo "Error getting 
 echo "-> Registering host..."
 
 #building api's json
-REGISTER=`curl --connect-timeout 20 -s -X POST -H "Content-Type: application/json" $URL  -d '{
+REGISTER=`curl --connect-timeout 15 -s -X POST -H "Content-Type: application/json" "$URL" -d '{
   "jsonrpc":"2.0",
   "method" : "host.create",
   "params":{
@@ -55,7 +51,7 @@ REGISTER=`curl --connect-timeout 20 -s -X POST -H "Content-Type: application/jso
       "dns":"'"$DNS"'",
       "port":10050,
       "groups":[
-          '"$GROUPS"'
+          '"$GRPS"'
       ],  
       "templates":[
           '"$TEMPLATES"'
@@ -65,5 +61,7 @@ REGISTER=`curl --connect-timeout 20 -s -X POST -H "Content-Type: application/jso
   "id":1
 }
 '`
+
+echo $REGISTER
 echo "$REGISTER"|grep -q '"hostids":\["[0-9]*"\]' || { echo "Error registering $DNS !!!!"; exit 0 ; }
 echo "-> $DNS registered !!"
--- chef/cookbooks/zabbix-aws/files/default/auto-register/zabbix_autoregister_wget.sh
+++ chef/cookbooks/zabbix-aws/files/default/auto-register/zabbix_autoregister_wget.sh
@@ -12,28 +12,28 @@ USER='user'
 PASSWORD='password'
 
 #machine's amazon informations
-DNS=`wget --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/hostname`
-ISID=`wget --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/instance-id`
+DNS=`wget --tries=1 --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/hostname`
+ISID=`wget --tries=1 --timeout=15 -q -O- http://169.254.169.254/latest/meta-data/instance-id`
 
 #testing amazon informations
 test -z "$DNS" && { echo "Erro ao pegar DNS"; exit 0; }
 test -z "$ISID" && { echo "Erro ao pegar Instance-id";  exit 0; }
 
 echo "-> Authenticating..."
-AUTH=`wget --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"user.authenticate","params":{"user":"'"$USER"'","password":"'"$PASSWORD"'"},"id": 1}' "$URL" |sed -n 's/.*"result":"\([a-f0-9]*\)".*/\1/p'`
+AUTH=`wget --tries=1 --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"user.authenticate","params":{"user":"'"$USER"'","password":"'"$PASSWORD"'"},"id": 1}' "$URL" |sed -n 's/.*"result":"\([a-f0-9]*\)".*/\1/p'`
 
 #Testing Auth
 test -z "$AUTH" && { echo "Error authenticating" ; exit 0 ; }
 
 echo "-> Searching hostgroups..."
 # From tag's group and Discovered hosts
-GRPS=`wget --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"hostgroup.get","params":{"filter":{"name": ["'"$MYTAG"'","Discovered hosts"]}},"id": 1,"auth":"'"$AUTH"'"}' "$URL"| sed -n 's/.*"result":\[\([^]]*\)\].*/\1/p'`
+GRPS=`wget --tries=1 --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"hostgroup.get","params":{"filter":{"name": ["'"$MYTAG"'","Discovered hosts"]}},"id": 1,"auth":"'"$AUTH"'"}' "$URL"| sed -n 's/.*"result":\[\([^]]*\)\].*/\1/p'`
 
 #checking groups 
 echo "$GRPS" |grep -o groupid|wc -l|grep -qx 2 || { echo "Error getting groups"; exit 0 ; }
 
 echo "-> Getting LinuxBasic and Tag's template..."
-TEMPLATES=`wget --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"template.get","params":{"filter":{"host":["Template_LinuxBasic","Template_'"$MYTAG"'"]}},"id": 1,"auth":"'"$AUTH"'"}' "$URL"| sed -n 's/.*"result":\[\([^]]*\)\].*/\1/;s/"hostid":"[^"]*",//gp'`
+TEMPLATES=`wget --tries=1 --timeout=15 -q -O- --header="Content-Type: application/json" --post-data='{"jsonrpc":"2.0","method":"template.get","params":{"filter":{"host":["Template_LinuxBasic","Template_'"$MYTAG"'"]}},"id": 1,"auth":"'"$AUTH"'"}' "$URL"| sed -n 's/.*"result":\[\([^]]*\)\].*/\1/;s/"hostid":"[^"]*",//gp'`
 
 #checking templates
 echo "$TEMPLATES" |grep -o templateid|wc -l|grep -qx 2 || { echo "Error getting templates"; exit 0 ; }
@@ -41,7 +41,7 @@ echo "$TEMPLATES" |grep -o templateid|wc -l|grep -qx 2 || { echo "Error getting 
 echo "-> Registering host..."
 
 #building api's json
-REGISTER=`wget --timeout=20 -q -O- --header="Content-Type: application/json" $URL --post-data='{
+REGISTER=`wget --tries=1 --timeout=20 -q -O- --header="Content-Type: application/json" $URL --post-data='{
   "jsonrpc":"2.0",
   "method" : "host.create",
   "params":{
